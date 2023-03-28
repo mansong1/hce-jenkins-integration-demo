007ca1935b2e497a39326998eee627b2fdc3198e
Karthik Satchitanand <karthik.s@harness.io>
2023-03-29 00:24:51 +0530

    Update rollback-deploy.sh

--- scripts/rollback-deploy.sh
+++ scripts/rollback-deploy.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
-if [ $1 -lt ${EXPECTED_RESILIENCE_SCORE} ]; then
+#remove double quotes on the value obtained from jq parse
+obtained_resilience_score=$(echo "$1" | tr -d '"')
+
+if [ $obtained_resilience_score -lt ${EXPECTED_RESILIENCE_SCORE} ]; then
   kubectl rollout undo deployment/cartservice -n boutique
+  exit 1
 fi 
