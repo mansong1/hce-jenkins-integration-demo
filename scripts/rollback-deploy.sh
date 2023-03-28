a97261c888c261253d6a645bb0c8a44201c20084
Karthik Satchitanand <karthik.s@harness.io>
2023-03-29 00:14:01 +0530

    Update rollback-deploy.sh

--- scripts/rollback-deploy.sh
+++ scripts/rollback-deploy.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-if [ "$1" -ge "${EXPECTED_RESILIENCE_SCORE}" ]; then
+if [ "$1" -lt "${EXPECTED_RESILIENCE_SCORE}" ]; then
   kubectl rollout undo deployment/cartservice -n boutique
 fi 
