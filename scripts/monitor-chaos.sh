186c9eb849a9a11d35675950d80ce318604fbb4b
Karthik Satchitanand <karthik.s@harness.io>
2023-03-28 19:49:03 +0530

    Create monitor-chaos.sh

---
+++ scripts/monitor-chaos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl -L https://github.com/uditgaurav/hce-api-template/releases/download/0.1.0-saas/hce-api-linux-amd64 -o hce-api-saas
+
+chmod +x hce-api
+
+./hce-api-saas generate --api monitor-experiment --account-id=${ACCOUNT_ID} \
+--project-id ${PROJECT_ID} --workflow-run-id ${WORKFLOW_RUN_ID} \
+--api-key ${API_KEY} --file-name hce-api.sh --timeout="300"
