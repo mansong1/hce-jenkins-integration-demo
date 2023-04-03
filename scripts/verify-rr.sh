ea297a0ac3faa2cb363c6e67b0ab5ecc7e91975f
Karthik Satchitanand <karthik.s@harness.io>
2023-04-03 15:47:17 +0530

    Update verify-rr.sh

--- scripts/verify-rr.sh
+++ scripts/verify-rr.sh
@@ -2,7 +2,8 @@
 
 set -e 
 
-curl -sL https://github.com/uditgaurav/hce-api-template/releases/download/0.1.0-saas/hce-api-linux-amd64 -o hce-api-saas
+#curl -sL https://github.com/uditgaurav/hce-api-template/releases/download/0.1.0-saas/hce-api-linux-amd64 -o hce-api-saas
+curl -sL https://storage.googleapis.com/hce-api/hce-api-linux-amd64 -o hce-api-saas
 
 chmod +x hce-api-saas
 
