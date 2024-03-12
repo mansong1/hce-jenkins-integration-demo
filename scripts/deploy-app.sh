40962943a91112c336b0a83c8b98385f0aa54edc
Karthik Satchitanand <karthik.s@harness.io>
2024-03-12 14:13:28 +0530

    Update deploy-app.sh

--- scripts/deploy-app.sh
+++ scripts/deploy-app.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sleep 10
-/var/jenkins_home/kubectl apply -f https://raw.githubusercontent.com/ksatchit/hce-jenkins-integration-demo/main/k8s/cartservice.yaml
+kubectl apply -f https://raw.githubusercontent.com/ksatchit/hce-jenkins-integration-demo/main/k8s/cartservice.yaml
 
 echo "waiting for deploy rollout to complete.."
 
-/var/jenkins_home/kubectl rollout status deployment cartservice -n boutique
+kubectl rollout status deployment cartservice -n boutique
 
