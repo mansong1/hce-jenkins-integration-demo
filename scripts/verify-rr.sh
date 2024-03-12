cccef32062e8fdd704b0a9b8317e758d7d7ca8d1
Karthik Satchitanand <karthik.s@harness.io>
2024-03-12 12:34:50 +0530

    Update verify-rr.sh

--- scripts/verify-rr.sh
+++ scripts/verify-rr.sh
@@ -3,11 +3,11 @@
 set -e 
 
 #curl -sL https://github.com/uditgaurav/hce-api-template/releases/download/0.1.0-saas/hce-api-linux-amd64 -o hce-api-saas
-curl -sL https://storage.googleapis.com/hce-api/hce-api-linux-amd64 -o hce-api-saas
+#curl -sL https://storage.googleapis.com/hce-api/hce-api-linux-amd64 -o hce-api-saas
 
-chmod +x hce-api-saas
+#chmod +x hce-api-saas
 
-resiliencyScore=$(./hce-api-saas generate --api validate-resilience-score  --account-id=${ACCOUNT_ID} \
+resiliencyScore=$(/var/jenkins_home/hce-cli generate --api validate-resilience-score  --account-id=${ACCOUNT_ID} \
 --project-id ${PROJECT_ID} --notifyID=$1  \
 --api-key ${API_KEY} --file-name hce-api.sh)
 
