a3f1b99227ca7267c32a5b6e32906cee7f55f986
Karthik Satchitanand <karthik.s@harness.io>
2024-03-12 12:33:30 +0530

    Update monitor-chaos.sh

--- scripts/monitor-chaos.sh
+++ scripts/monitor-chaos.sh
@@ -3,10 +3,10 @@
 set -e
 
 #curl -sL https://github.com/uditgaurav/hce-api-template/releases/download/0.1.0-saas/hce-api-linux-amd64 -o hce-api-saas
-curl -sL https://storage.googleapis.com/hce-api/hce-api-linux-amd64 -o hce-api-saas
+#curl -sL https://storage.googleapis.com/hce-api/hce-api-linux-amd64 -o hce-api-saas
 
-chmod +x hce-api-saas
+#chmod +x hce-api-saas
 
-./hce-api-saas generate --api monitor-experiment --account-id=${ACCOUNT_ID} \
+/var/jenkins_home/hce-cli generate --api monitor-experiment --account-id=${ACCOUNT_ID} \
 --project-id ${PROJECT_ID} --notifyID=$1  \
 --api-key ${API_KEY} --file-name hce-api.sh --timeout="500"
