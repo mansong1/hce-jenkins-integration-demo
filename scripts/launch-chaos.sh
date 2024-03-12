3b0b19ff489e339785625f3e8e3a5ff8a9e73d5a
Karthik Satchitanand <karthik.s@harness.io>
2024-03-12 12:32:02 +0530

    Update launch-chaos.sh

--- scripts/launch-chaos.sh
+++ scripts/launch-chaos.sh
@@ -3,13 +3,13 @@
 set -e
 
 #curl -sL https://github.com/uditgaurav/hce-api-template/releases/download/0.1.0-saas/hce-api-linux-amd64 -o hce-api-saas
-curl -sL https://storage.googleapis.com/hce-api/hce-api-linux-amd64 -o hce-api-saas
+#curl -sL https://storage.googleapis.com/hce-api/hce-api-linux-amd64 -o hce-api-saas
 
-chmod +x hce-api-saas
+#chmod +x hce-api-saas
 
-output=$(./hce-api-saas generate --api launch-experiment --account-id=${ACCOUNT_ID} \
+output=$(/var/jenkins_home/hce-cli generate --api launch-experiment --account-id=${ACCOUNT_ID} \
 --project-id ${PROJECT_ID} --workflow-id ${WORKFLOW_ID} \
---api-key ${API_KEY} --file-name hce-api.sh | jq -r '.data.runChaosExperiment.notifyID')
+--api-key ${API_KEY} --file-name hce-api.sh | /var/jenkins_home/jq -r '.data.runChaosExperiment.notifyID')
 
 echo ${output}
 
